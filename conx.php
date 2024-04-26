6c5a33ff82104d637f32b38dff35643cf84dd758
Ristence <joaquin.alarconroa@gmail.com>
2024-04-25 20:38:28 -0400

    A

--- conx.php
+++ conx.php
@@ -4,11 +4,17 @@
 
 
 $_server = "localhost";
-$_user = "Admin";
-$_pass = "Admin";
+$_user = "root";
+$_pass = "";
 $_db = "sistemaot";
-
 echo "bien";
 
+$conexion = new mysqli($_server,$_user,$_pass,$_db);
 
+
+if($conexion->connect_errno){
+    die("malo" . $conexion->connect_error());
+}else{
+    echo "bien";
+}
 ?>
